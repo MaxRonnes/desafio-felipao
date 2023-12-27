938e4e1da7b11452f80fa21fb4ba5c50cfe86753
maxronney <maxronney94@gmail.com>
2023-12-27 02:44:21 -0300

    "Commit Final"

--- indexxx.js
+++ indexxx.js
@@ -1,7 +1,20 @@
+var Herói = "Max"
+var xp = 4560
 
-let nome = "max";
-
-let cachorro
-
-
-console.log("Max_Ronnes");
+if(xp <= 1000){
+console.log("O Herói de nome " + Herói + " está no nivel de Ferro")
+}else if(xp >= 1.001 && xp <= 2000){
+    console.log("O Herói de nome " + Herói + " está no nivel de Bronze")
+}else if(xp >= 2001 && xp <= 5000){
+    console.log("O Herói de nome " + Herói + " está no nivel de Prata")
+}else if(xp >= 6001 && xp <= 7000){
+    console.log("O Herói de nome " + Herói + " está no nivel de Ouro")
+}else if(xp >= 7001 && xp <= 8000){
+    console.log("O Herói de nome " + Herói + " está no nivel de Platina")
+}else if(xp >= 8001 && xp <= 9000){
+console.log("O Herói de nome " + Herói + " está no nivel de Ascendente")
+}else if(xp >= 9001 && xp <= 10000){
+    console.log("O Herói de nome " + Herói + " está no nivel de Imortal")
+}else if(xp >= 10001){
+    console.log("O Herói de nome " + Herói + " está no nivel de Radiante")
+}
