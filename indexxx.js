6097ebb90daaf5f447e66b5ad4d7a2cf3e3d1e1f
MaxRonnes <129607382+MaxRonnes@users.noreply.github.com>
2023-12-29 12:27:16 -0300

    Inclusão de uma CONST - Resultado final do Projeto
    Inclusão da const "Entrada" pois foi esta forma que consegui incluir a entrada de dados via teclado para serem visualizadas no terminal da IDE VS CODE.

--- indexxx.js
+++ indexxx.js
@@ -1,23 +1,27 @@
-var Herói = "Max"
-var xp = 7500
+const entrada = require('prompt-sync')({sigint: true});
+
+var Herói = entrada("Informe o nome do Herói: ")
+var xp = entrada("Informe o XP do Herói: ")
+
+
 
 for (var i = 0; i <= xp; i++) {
     
 }
-if (i <= 1000) {
-    console.log("O Herói de nome " + Herói + " está no nível de Ferro");
-}else if (i <= 2000) {
-        console.log("O Herói de nome " + Herói + " está no nível de Bronze");
-    }else if (i <= 5000) {
-        console.log("O Herói de nome " + Herói + " está no nível de Prata");
-    } else if (i <= 7000) {
-        console.log("O Herói de nome " + Herói + " está no nível de Ouro");
-    } else if (i <= 8000) {
-        console.log("O Herói de nome " + Herói + " está no nível de Platina");
-    } else if (i <= 9000) {
-        console.log("O Herói de nome " + Herói + " está no nível de Ascendente");
-    } else if (i <= 10000) {
-        console.log("O Herói de nome " + Herói + " está no nível de Imortal");
-    } else {
-        console.log("O Herói de nome " + Herói + " está no nível de Radiante");
+if(xp <= 1000){
+    console.log("O Herói de nome " + Herói + " está no nivel de Ferro")
+    }else if(xp >= 1.001 && xp <= 2000){
+        console.log("O Herói de nome " + Herói + " está no nivel de Bronze")
+    }else if(xp >= 2001 && xp <= 5000){
+        console.log("O Herói de nome " + Herói + " está no nivel de Prata")
+    }else if(xp >= 6001 && xp <= 7000){
+        console.log("O Herói de nome " + Herói + " está no nivel de Ouro")
+    }else if(xp >= 7001 && xp <= 8000){
+        console.log("O Herói de nome " + Herói + " está no nivel de Platina")
+    }else if(xp >= 8001 && xp <= 9000){
+    console.log("O Herói de nome " + Herói + " está no nivel de Ascendente")
+    }else if(xp >= 9001 && xp <= 10000){
+        console.log("O Herói de nome " + Herói + " está no nivel de Imortal")
+    }else if(xp >= 10001){
+        console.log("O Herói de nome " + Herói + " está no nivel de Radiante")
     }
